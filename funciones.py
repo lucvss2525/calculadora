e3f4ab32d61649a92256dd9fdf2e1c8da601f45a
NetAcad DEVASC <changeme@example.com>
2025-04-04 15:40:03 +0000

    first commit

---
+++ funciones.py
@@ -0,0 +1,8 @@
+def suma (a,b):
+    print(a + b)
+def resta (a,b):
+    print(a - b)
+def multiplicacion (a,b):
+    print(a * b)
+def division (a,b):
+    print(a / b)
---
+++ main.py
@@ -0,0 +1,38 @@
+#calculadora#
+from funciones import suma, resta, multiplicacion, division
+
+print("""
+    ------
+    suma 1
+    -------
+    resta 2
+    -------
+    multiplicacion 3
+    ----------------
+    division 4
+    ----------
+    """)
+
+M = int(input('ingrese una opcion :'))
+
+if M == 1:
+    print ('Ud eligio  suma')
+    A = int (input('Ingrese Numero 1: '))
+    B = int (input('Ingrese Numero 2: '))
+    suma(A,B)
+elif M == 2:
+    print ('Ud eligio  la Resta')
+    A = int (input('Ingrese Numero 1: '))
+    B = int (input('Ingrese Numero 2: '))
+    resta(A,B)
+elif M == 3:
+    print ('Ud eligio  la Multiplicacion')
+    A = int (input('Ingrese Numero 1: '))
+    B = int (input('Ingrese Numero 2: '))
+    multiplicacion(A,B)
+elif M == 4:
+    print ('Ud eligio  la Division')
+    A = int (input('Ingrese Numero 1: '))
+    B = int (input('Ingrese Numero 2: '))
+    division(A,B)
+    
